1a4d4db5bbdffbbd16653a048f83aa744f15c6da
Amar <amaratasia@gmail.com>
2021-06-30 01:11:12 +0530

    Notif

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ApplicationRecord
   has_many :order_items
   has_one :shipping_address
   delegate :name, to: :shipping_address
+
+  after_create do
+    SmsNotification.new({name: shipping_address.name, order_number: store_order_id, order_total: total}, shipping_address.phone).call
+  end
 end
